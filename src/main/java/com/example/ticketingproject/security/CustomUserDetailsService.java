183fa16fd4dc1f886d2772b3e106d461cfa408c0
bjw446 <bjw446@naver.com>
2026-03-06 20:47:00 +0900

    init

--- src/main/java/com/example/ticketingproject/security/CustomUserDetailsService.java
+++ src/main/java/com/example/ticketingproject/security/CustomUserDetailsService.java
@@ -19,6 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
+
         return new CustomUserDetails(
                 user.getId(),
                 user.getEmail(),
