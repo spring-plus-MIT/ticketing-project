f234693505f4c4c599e5e897a01813a5bcc98197
bjw446 <bjw446@naver.com>
2026-03-07 19:10:34 +0900

    refactor : jwt 토큰 시크릿키 방식 수정

--- src/main/java/com/example/ticketingproject/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/ticketingproject/security/jwt/JwtTokenProvider.java
@@ -2,15 +2,25 @@ package com.example.ticketingproject.security.jwt;
 
 import com.example.ticketingproject.security.CustomUserDetails;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
 
-    private final String SECRET = "ticketing-project-test-secretKey";
-    private final long EXPIRATION = 1000 * 60 * 60;
+    @Value("${jwt.secret.key}")
+    private String SECRET;
+    @Value("${jwt.expiration}")
+    private long EXPIRATION;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
 
     public String createToken(CustomUserDetails userDetails) {
 
@@ -18,15 +28,17 @@ public class JwtTokenProvider {
                 .setSubject(userDetails.getEmail())
                 .claim("userId", userDetails.getId())
                 .claim("role", userDetails.getUserRole().name())
+                .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public Claims getClaims(String token) {
 
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
