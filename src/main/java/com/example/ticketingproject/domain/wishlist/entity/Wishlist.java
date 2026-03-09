df5a5a349051f058193e3816c58927007aa5ea34
minky5004 <minky5004@gmail.com>
2026-03-09 14:29:31 +0900

    fix:오타수정

--- src/main/java/com/example/ticketingproject/domain/wishlist/entity/Wishlist.java
+++ src/main/java/com/example/ticketingproject/domain/wishlist/entity/Wishlist.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "wishlists")
 public class Wishlist extends CreatableEntity{
 
-    @id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
