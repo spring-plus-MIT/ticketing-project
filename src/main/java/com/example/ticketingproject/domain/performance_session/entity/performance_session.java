aaeb9ff64d5b1a6d975d9a9298d9e5131053cee7
minky5004 <minky5004@gmail.com>
2026-03-09 14:05:37 +0900

    feature:ãperformance_session,wishlist 엔티티추가

---
+++ src/main/java/com/example/ticketingproject/domain/performance_session/entity/performance_session.java
@@ -0,0 +1,31 @@
+package com.example.ticketingproject.domain.performance_session.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "performance_sessions")
+public class performance_session extends DeletableEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "performance_id", nullable = false)
+    private Performance performance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+
+    private LocalDateTime session_date;
+    private LocalDateTime session_time;
+
+}
---
+++ src/main/java/com/example/ticketingproject/domain/wishlist/entity/wishlist.java
@@ -0,0 +1,27 @@
+package com.example.ticketingproject.domain.wishlist.entity;
+
+import com.example.ticketingproject.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "wishlists")
+public class wishlist extends CreatableEntity{
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "work_id", nullable = false)
+    private Work work;
+
+}
