6ca682d0e885222e97a20bf798bbb956fc99411d
김세현 <sehyun@gimsehyeon-ui-MacBookAir.local>
2026-03-10 17:53:56 +0900

    refactor: RequiredArgsConstructor 추가

--- src/main/java/com/example/ticketingproject/domain/venue/service/VenueService.java
+++ src/main/java/com/example/ticketingproject/domain/venue/service/VenueService.java
@@ -3,19 +3,17 @@ package com.example.ticketingproject.domain.venue.service;
 import com.example.ticketingproject.domain.venue.dto.VenueResponse;
 import com.example.ticketingproject.domain.venue.entity.Venue;
 import com.example.ticketingproject.domain.venue.repository.VenueRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class VenueService {
     private final VenueRepository venueRepository;
-
-    public VenueService(VenueRepository venueRepository) {
-        this.venueRepository = venueRepository;
-    }
 
     public Page<VenueResponse> getVenues(Pageable pageable) {
         Page<Venue> venues = venueRepository.findAll(pageable);
