9da864ca0c64b2da1e41eea44582a6fba4e18558
minky5004 <minky5004@gmail.com>
2026-03-10 11:25:46 +0900

    refactor:임의 추가한 succes코드 삭제

--- src/main/java/com/example/ticketingproject/common/enums/SuccessStatus.java
+++ src/main/java/com/example/ticketingproject/common/enums/SuccessStatus.java
@@ -11,11 +11,7 @@ public enum SuccessStatus {
     LOGIN_SUCCESS(HttpStatus.OK, "200_LOGIN_SUCCESS", "로그인에 성공하였습니다."),
     FOUND_SUCCESS(HttpStatus.OK, "200_FOUND_SUCCESS", "데이터 조회에 성공하였습니다."),
     LOGOUT_SUCCESS(HttpStatus.OK, "200_LOGOUT_SUCCESS", "로그아웃에 성공하였습니다."),
-    DELETE_SUCCESS(HttpStatus.OK, "200_DELETE_SUCCESS", "삭제에 성공하였습니다."),
-    CREATED(HttpStatus.OK,"200_CREATED_SUCCES","생성됨"),
-    GET_SUCCESS(HttpStatus.OK,"200_GET_SUCCES","조회 성공"),
-    PROCESS_SUCCESS(HttpStatus.OK,"200_PROCESS_SUCCESS","절차 성공");
-
+    DELETE_SUCCESS(HttpStatus.OK, "200_DELETE_SUCCESS", "삭제에 성공하였습니다.");
 
     private final HttpStatus httpStatus;
     private final String successCode;
