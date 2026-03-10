8e7b07661b9a90472cba401c9b83806587c31698
bjw446 <bjw446@naver.com>
2026-03-10 11:43:13 +0900

    feat : Validation 글로벌 익셉션 핸들러 추가 및 공통 응답 적용

--- src/main/java/com/example/ticketingproject/common/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/ticketingproject/common/config/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.example.ticketingproject.common.config;
 
 import com.example.ticketingproject.common.dto.ErrorResponse;
+import com.example.ticketingproject.common.enums.ErrorStatus;
 import com.example.ticketingproject.common.exception.BaseException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,5 +24,30 @@ public class GlobalExceptionHandler {
                 request.getRequestURI()
         );
         return ResponseEntity.status(e.getErrorStatusCode().getHttpStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // DTO @Valid 에러
+    public ResponseEntity<ErrorResponse> MethodArgumentNotValidExceptionHandle(
+            MethodArgumentNotValidException e, HttpServletRequest request
+    ) {
+        return getValidationErrorResponse(request);
+    }
+
+    // @Validated가 붙은 @RequestParam, @PathVariable 에러
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> ConstraintViolationExceptionHandle(
+            ConstraintViolationException e, HttpServletRequest request
+    ) {
+        return getValidationErrorResponse(request);
+    }
+
+    public ResponseEntity<ErrorResponse> getValidationErrorResponse(HttpServletRequest request) {
+        ErrorResponse errorResponse = ErrorResponse.of(
+                ErrorStatus.VALIDATION_ERROR.getHttpStatus(),
+                ErrorStatus.VALIDATION_ERROR.getErrorCode(),
+                ErrorStatus.VALIDATION_ERROR.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(ErrorStatus.VALIDATION_ERROR.getHttpStatus()).body(errorResponse);
     }
 }
