0d90bedc24adfe64abcf3b4c2feb20e6f9aaf59f
bjw446 <bjw446@naver.com>
2026-03-11 12:41:46 +0900

    feat : Work 생성, 목록 조회, 단건 조회, 수정 추가

--- src/main/java/com/example/ticketingproject/common/dto/CommonResponse.java
+++ src/main/java/com/example/ticketingproject/common/dto/CommonResponse.java
@@ -24,14 +24,4 @@ public class CommonResponse<T> {
                 data
         );
     }
-
-        public static <T> CommonResponse<T> success(T data) {
-        return new CommonResponse<>(
-                LocalDateTime.now(),
-                HttpStatus.OK,
-                "SUCCESS",
-                "요청에 성공하였습니다.",
-                data
-        );
-    }
 }
