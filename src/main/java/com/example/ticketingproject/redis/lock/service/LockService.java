75bda997a14597c7b43c42d39e8eacbb11418644
bjw446 <bjw446@naver.com>
2026-03-15 18:34:06 +0900

    feat : 좌석 생성 Lock Retry 방식 Exponential + Jitter 로 변경 docs : 좌석 생성 동시성 시나리오 README에 추가

--- src/main/java/com/example/ticketingproject/redis/lock/service/LockService.java
+++ src/main/java/com/example/ticketingproject/redis/lock/service/LockService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
@@ -35,6 +36,10 @@ public class LockService {
 
         int retry = 10;
 
+        // Fixed Backoff -> Exponential (지수적으로 점점 증가) + Jitter (랜덤) 방식으로 수정
+        int baseDelay = 10; // 기본 대기 시간
+        int attempt = 0; // 재시도 횟수
+
         while (retry > 0) {
             Boolean success = lockRedisRepository.tryLock(key, uuid, LOCK_TTL);
 
@@ -43,12 +48,18 @@ public class LockService {
             }
 
             try {
-                Thread.sleep(50);
+                // Exponential Backoff 공식 delay = baseDelay * 2^attempt(2의 재시도 횟수만큼 제곱)
+                long delay = baseDelay * (1L << attempt); // '<<' 비트 시프트 연산자 (2의 attempt 제곱)
+                long jitter = ThreadLocalRandom.current().nextLong(10); // 0~9ms 랜덤 시간 추가
+
+                Thread.sleep(delay + jitter); // 재시도 전 대기 시간 수정 // 기존 10 = Fixed Backoff
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
 
             retry--;
+
+            attempt++;
         }
 
         throw new LockException(
