596c6aff23eddf1e5c729eb1252b7f2f39db6696
김세현 <sehyun@gimsehyeon-ui-MacBookAir.local>
2026-03-18 10:54:55 +0900

    fix: SuperAdminInitializer bean을 테스트 대상에서 제외

--- src/main/java/com/example/ticketingproject/common/config/SuperAdminInitializer.java
+++ src/main/java/com/example/ticketingproject/common/config/SuperAdminInitializer.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,7 @@ import java.math.BigDecimal;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Profile("!test")
 public class SuperAdminInitializer implements ApplicationRunner {
 
     private final UserRepository userRepository;
