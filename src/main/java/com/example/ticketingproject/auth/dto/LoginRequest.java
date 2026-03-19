105b034f863378c5faa77019818d850c5a62b8b2
김세현 <sehyun@gimsehyeon-ui-MacBookAir.local>
2026-03-19 15:40:07 +0900

    refactor: 로그인/회원가입 시 유효성 검증 조건 통일

--- src/main/java/com/example/ticketingproject/auth/dto/LoginRequest.java
+++ src/main/java/com/example/ticketingproject/auth/dto/LoginRequest.java
@@ -1,11 +1,10 @@
 package com.example.ticketingproject.auth.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
-import static com.example.ticketingproject.common.util.Constants.*;
+import static com.example.ticketingproject.common.util.Constants.MSG_VALIDATION_EMAIL_ERROR;
 
 @Getter
 public class LoginRequest {
@@ -13,7 +12,7 @@ public class LoginRequest {
     @Email(message = MSG_VALIDATION_EMAIL_ERROR)
     private String email;
 
-    @Length(min = 8, max = 20, message = MSG_VALIDATION_LENGTH_ERROR)
+    @Length(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     private String password;
 
 }
