fffb53ca183380067c5ea55b63a626afc513a979
minky5004 <minky5004@gmail.com>
2026-03-19 14:41:22 +0900

    refactorLvalid 정규식 리팩토링

--- src/main/java/com/example/ticketingproject/domain/payment/entity/Payment.java
+++ src/main/java/com/example/ticketingproject/domain/payment/entity/Payment.java
@@ -5,6 +5,9 @@ import com.example.ticketingproject.domain.payment.enums.PaymentStatus;
 import com.example.ticketingproject.domain.reservation.entity.Reservation;
 import com.example.ticketingproject.domain.user.entity.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -18,30 +21,38 @@ public class Payment extends CreatableEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "reservation_id", nullable = false)
     private Reservation reservation;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @JoinColumn(nullable = false)
+    @NotNull
+    @DecimalMin(value = "0.0")
+    @Digits(integer = 8, fraction = 0)
+    @Column(nullable = false, precision = 8, scale = 0)
     private BigDecimal amount;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PaymentStatus paymentStatus;
 
+    @NotNull
+    @DecimalMin(value = "0.0")
+    @Column(nullable = false)
     private BigDecimal balanceAfterPayment;
 
     @Builder
-    public Payment(Reservation reservation, User user, BigDecimal amount, PaymentStatus paymentStatus,BigDecimal balanceAfterPayment) {
+    public Payment(Reservation reservation, User user, BigDecimal amount, PaymentStatus paymentStatus, BigDecimal balanceAfterPayment) {
         this.reservation = reservation;
         this.user = user;
         this.amount = amount;
         this.paymentStatus = paymentStatus;
         this.balanceAfterPayment = balanceAfterPayment;
     }
-
-
 }
--- src/main/java/com/example/ticketingproject/domain/performance/dto/PerformanceRequest.java
+++ src/main/java/com/example/ticketingproject/domain/performance/dto/PerformanceRequest.java
@@ -1,16 +1,33 @@
 package com.example.ticketingproject.domain.performance.dto;
 
 import com.example.ticketingproject.domain.performance.enums.PerformanceStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
+import static com.example.ticketingproject.common.util.Constants.MSG_VALIDATION_NOT_NULL_ERROR;
+
 @Getter
 public class PerformanceRequest {
+
+    @NotNull(message = MSG_VALIDATION_NOT_NULL_ERROR)
     private Long workId;
+
+    @NotNull(message = MSG_VALIDATION_NOT_NULL_ERROR)
     private Long venueId;
+
+    @NotNull(message = MSG_VALIDATION_NOT_NULL_ERROR)
+    @Length(min = 1, max = 20)
     private String season;
+
+    @NotNull(message = MSG_VALIDATION_NOT_NULL_ERROR)
     private LocalDate startDate;
+
+    @NotNull(message = MSG_VALIDATION_NOT_NULL_ERROR)
     private LocalDate endDate;
+
+    @NotNull(message = MSG_VALIDATION_NOT_NULL_ERROR)
     private PerformanceStatus status;
 }
