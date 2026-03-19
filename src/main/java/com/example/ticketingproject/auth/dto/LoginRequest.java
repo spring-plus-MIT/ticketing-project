8b8ab9efc06ce766f2efac784a2502b749233de3
김세현 <sehyun@gimsehyeon-ui-MacBookAir.local>
2026-03-19 15:31:19 +0900

    refactor: LoginRequest dto validation 수정

--- src/main/java/com/example/ticketingproject/auth/dto/LoginRequest.java
+++ src/main/java/com/example/ticketingproject/auth/dto/LoginRequest.java
@@ -10,11 +10,9 @@ import static com.example.ticketingproject.common.util.Constants.*;
 @Getter
 public class LoginRequest {
 
-    @NotBlank(message = MSG_VALIDATION_NOT_BLANK_ERROR)
     @Email(message = MSG_VALIDATION_EMAIL_ERROR)
     private String email;
 
-    @NotBlank(message = MSG_VALIDATION_NOT_BLANK_ERROR)
     @Length(min = 8, max = 20, message = MSG_VALIDATION_LENGTH_ERROR)
     private String password;
 
