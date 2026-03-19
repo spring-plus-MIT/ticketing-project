4ab390bc0003cabce661472b30c297c2494f25e2
김세현 <sehyun@gimsehyeon-ui-MacBookAir.local>
2026-03-19 14:23:21 +0900

    refactor: CastMember entity, dto 조건 추가

--- src/main/java/com/example/ticketingproject/domain/castmember/dto/CastMemberRequest.java
+++ src/main/java/com/example/ticketingproject/domain/castmember/dto/CastMemberRequest.java
@@ -1,9 +1,14 @@
 package com.example.ticketingproject.domain.castmember.dto;
 
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 public class CastMemberRequest {
+
+    @Length(min = 1, max = 50, message = "출연자명은 1자 이상 50자 이하만 가능합니다.")
     private String name;
+
+    @Length(min = 1, max = 50, message = "출연자명은 1자 이상 50자 이하만 가능합니다.")
     private String roleName;
 }
