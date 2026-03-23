9b318b7cfd4c6a29766f9539c5990593133ddfcb
Leejun14 <777ljy777@gmail.com>
2026-03-23 12:49:20 +0900

    fix: 검색 querydsl 구조 변경 및 반환 데이터 수정

--- src/main/java/com/example/ticketingproject/domain/performancesession/repository/PerformanceSessionCustomRepository.java
+++ src/main/java/com/example/ticketingproject/domain/performancesession/repository/PerformanceSessionCustomRepository.java
@@ -7,10 +7,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface PerformanceSessionCustomRepository {
 
     Page<PerformanceSearchResponse> searchPerformance(
+            String keyword,
+            Category category,
+            LocalDate startDate,
+            LocalDate endDate,
+            PerformanceStatus status,
+            Pageable pageable);
+
+    List<PerformanceSearchResponse> searchPerformanceContent(
             String keyword,
             Category category,
             LocalDate startDate,
